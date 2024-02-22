51e00bbbebd1a924af15c1edbbdd6e537d18340a
MichaelRyu2000 <ryumichael2000@gmail.com>
2024-02-22 12:49:05 -0800

    Added scaffold for both applist and app screens along with a topappbar. The topappbar for app screen allows the user to navigate back to the app list screen and also open an intent to open the app info in the Settings app. Also added a lifecycle event observer that updates the permissions if the user changes them and the app is opened back to the foreground.

--- app/src/main/java/com/example/discoverpermissions/presentation/AppListScreen.kt
+++ app/src/main/java/com/example/discoverpermissions/presentation/AppListScreen.kt
@@ -8,15 +8,20 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Card
+import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarColors
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
 import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
@@ -63,27 +68,37 @@ fun AppListScreen(
     appList: List<ApplicationInfo>,
     modifier: Modifier = Modifier
 ) {
-
-//    TopAppBar(
-//        colors = TopAppBarDefaults.mediumTopAppBarColors(
-//            containerColor = MaterialTheme.colorScheme.primary,
-//            titleContentColor = MaterialTheme.colorScheme.onPrimary,
-//            navigationIconContentColor = MaterialTheme.colorScheme.onPrimary,
-//            actionIconContentColor = MaterialTheme.colorScheme.onSecondary
-//        ),
-//        title = { Text(text = "Applications") }
-//    )
-    LazyColumn( content = {
-        items(appList.size) {
-            AppPreviewCard(
-                packageManager = packageManager,
-                applicationInfo = appList[it],
-                navController = navController,
-                modifier = Modifier
-                    .height(128.dp)
-                    .padding(16.dp)
+    Scaffold(
+        topBar = {
+            CenterAlignedTopAppBar(
+                colors = TopAppBarDefaults.smallTopAppBarColors(
+                    containerColor = MaterialTheme.colorScheme.primaryContainer,
+                ),
+                title = {
+                    Text(
+                        text = "Applications",
+                        fontSize = 24.sp,
+                        fontWeight = FontWeight.Bold,
+                        textAlign = TextAlign.Center
+                    )
+                }
             )
         }
-    },
-    )
+    ) { innerPadding ->
+        LazyColumn( content = {
+            items(appList.size) {
+                AppPreviewCard(
+                    packageManager = packageManager,
+                    applicationInfo = appList[it],
+                    navController = navController,
+                    modifier = Modifier
+                        .height(128.dp)
+                        .padding(16.dp)
+                )
+            }
+        },
+            modifier = Modifier
+                .padding(innerPadding)
+        )
+    }
 }
